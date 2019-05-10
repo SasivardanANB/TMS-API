51d1975936a7582a1616bb873668ebfb24042115
Poorna Sekhar <sekharch377@gmail.com>
2019-05-10 10:17:17 +0530

    PIC status message added for delete

--- TMS.DataGateway/Repositories/PIC.cs
+++ TMS.DataGateway/Repositories/PIC.cs
@@ -98,11 +98,13 @@ namespace TMS.DataGateway.Repositories
                         context.SaveChanges();
                         picResponse.Status = DomainObjects.Resource.ResourceData.Success;
                         picResponse.StatusCode = (int)HttpStatusCode.OK;
+                        picResponse.StatusMessage = DomainObjects.Resource.ResourceData.PicDeleted;
                     }
                     else
                     {
-                        picResponse.Status = DomainObjects.Resource.ResourceData.Failure;
+                        picResponse.Status = DomainObjects.Resource.ResourceData.Success;
                         picResponse.StatusCode = (int)HttpStatusCode.NotFound;
+                        picResponse.StatusMessage = DomainObjects.Resource.ResourceData.InvalidPICID;
                     }
                 }
 
