728984fca99f717d6779bd061ca6b82931b1df4f
Sumit Kumar Pathak <sumit.kumar@anblicks.com>
2019-05-16 14:47:30 +0530

    TMS - Get Order completed

--- TMS.API/Controllers/OrderController.cs
+++ TMS.API/Controllers/OrderController.cs
@@ -106,7 +106,7 @@ namespace TMS.API.Controllers
         }
 
         [Route("getorders")]
-        [HttpPost]
+        [AllowAnonymous, HttpPost]
         public IHttpActionResult GetOrders(OrderSearchRequest orderSearchRequest)
         {
             IOrderTask orderTask = Helper.Model.DependencyResolver.DependencyResolver.GetImplementationOf<ITaskGateway>().OrderTask;
