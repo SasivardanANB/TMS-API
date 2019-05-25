9e616f5a9e3dcc6e8922384ab20224e401829771
Vinay Kumar Gundavaram <vinay.gundavaram@anblicks.com>
2019-05-25 12:35:27 +0530

    TMS SAMA Integration

---
+++ TMS.DomainObjects/Objects/SAMAUser.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TMS.DomainObjects.Objects
+{
+    public class SAMAUser
+    {
+        public int ID { get; set; }
+        public string UserName { get; set; }
+        public string CompanyCode { get; set; }
+        public string RegionCode { get; set; }
+        public string BranchCode { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string MobilePhone { get; set; }
+    }
+}
