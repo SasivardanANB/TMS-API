95b71533c58226d8ae5606cde842db7b7981a055
Vinay Kumar Gundavaram <vinay.gundavaram@anblicks.com>
2019-08-01 14:56:00 +0530

    Media BusinessGateway

--- TMS.API/Controllers/MediaController.cs
+++ TMS.API/Controllers/MediaController.cs
@@ -29,45 +29,6 @@ namespace TMS.API.Controllers
                 IMediaTask mediaTask = Helper.Model.DependencyResolver.DependencyResolver.GetImplementationOf<ITaskGateway>().MediaTask;
                 ResponseDataForFileUpload response = await mediaTask.UploadFile(Request);
                 return Ok(response);
-
-                //ResponseDataForFileUpload responseDataForFileUpload = new ResponseDataForFileUpload();
-                //if (!Request.Content.IsMimeMultipartContent("form-data"))
-                //{
-                //    throw new HttpResponseException(HttpStatusCode.UnsupportedMediaType);
-                //}
-                //var accountName = ConfigurationManager.AppSettings["storage:AccountName"];
-                //var accountKey = ConfigurationManager.AppSettings["storage:AccountKey"];
-                //var containerName = ConfigurationManager.AppSettings["storage:AccountContainer"];
-                //var storageAccount = new CloudStorageAccount(new StorageCredentials(accountName, accountKey), true);
-                //CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
-                //CloudBlobContainer blobContainer = blobClient.GetContainerReference(containerName);
-                //var provider = new AzureStorageMultipartFormDataStreamProvider(blobContainer);
-                //try
-                //{
-                //    await Request.Content.ReadAsMultipartAsync(provider);
-                //}
-                //catch (Exception ex)
-                //{
-                //    responseDataForFileUpload.Status = DomainObjects.Resource.ResourceData.Failure;
-                //    responseDataForFileUpload.StatusMessage = ex.Message;
-                //    responseDataForFileUpload.StatusCode = (int)HttpStatusCode.ExpectationFailed;
-                //    return Ok(responseDataForFileUpload);
-                //}
-
-                //// Retrieve the filename of the file you have uploaded
-                //var fileGuid = provider.FileData.FirstOrDefault()?.LocalFileName;
-                //if (string.IsNullOrEmpty(fileGuid))
-                //{
-                //    responseDataForFileUpload.Status = DomainObjects.Resource.ResourceData.Failure;
-                //    responseDataForFileUpload.StatusMessage = DomainObjects.Resource.ResourceData.FileUploadError;
-                //    responseDataForFileUpload.StatusCode = (int)HttpStatusCode.BadRequest;
-                //    return Ok(responseDataForFileUpload);
-                //}
-                //responseDataForFileUpload.Status = DomainObjects.Resource.ResourceData.Success;
-                //responseDataForFileUpload.StatusMessage = DomainObjects.Resource.ResourceData.FileUploadSuccess;
-                //responseDataForFileUpload.StatusCode = (int)HttpStatusCode.OK;
-                //responseDataForFileUpload.Guid = fileGuid;
-                //return Ok(responseDataForFileUpload);
             }
 
             [AllowAnonymous, HttpGet, Route("downloadfile")]
